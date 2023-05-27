f2832577dfca6c822fe54a507a408a52f2b4544b
Jimmy Widianto <wijayajimmywidianto@gmail.com>
2023-05-27 17:14:26 +0700

    create Home Activity

---
+++ app/src/main/java/com/example/capstone/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.capstone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
+
+// TODO Intent dari 3 button ke tampilan barang yang dicari
+// TODO ganti nama toko
